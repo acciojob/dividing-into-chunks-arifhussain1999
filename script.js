a25e7869db4495fdb05e374f06897e9400cc0785
Arif Hussain <arifh9821@gmail.com>
2024-08-14 01:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
-	let result = []; // To store the list of subarrays
-  let currentSubarray = []; // To store the current subarray
-  let currentSum = 0; // To track the sum of the current subarray
+  let result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
 
-	for (let num of arr) {
-		 // Check if the current number can be added to the current subarray
-		if(currentSum+=num<=n){
-			currentSubarray.push(num);
-			currentSum+=num;
-		}
-		else{
-			// If not, add the current subarray to the result
-			result.push(currentSubarray);
-			// Start a new subarray with the current number
-           currentSubarray = [num];
-           currentSum = num;
-		}
-	}
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
 
-	// Add the last subarray if it's not empty
   if (currentSubarray.length > 0) {
     result.push(currentSubarray);
   }
-  
+
   return result;
 };
 
-const n = prompt("Enter n: ");
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+// Stub for user input
+const n = parseInt(prompt("Enter n: "), 10);
 alert(JSON.stringify(divide(arr, n)));
